27cdaf64b6124ec6045337c8b1940093e851f2ef
oji <mlbbsaya10@gmail.com>
2025-08-04 16:04:42 +0700

    Create App.jsx

---
+++ server/client/src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import VerifyEmail from './pages/VerifyEmail';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
+import Dashboard from './pages/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/verify-email" element={<VerifyEmail />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/dashboard" element={<PrivateRoute />}>
+            <Route index element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </div>
+      <ToastContainer position="bottom-right" autoClose={5000} />
+    </BrowserRouter>
+  );
+}
+
+export default App;
