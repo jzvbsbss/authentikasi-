254d9e02805fd29d3f7d3575f5e075828b83a49b
oji <mlbbsaya10@gmail.com>
2025-08-04 16:09:49 +0700

    Create toast.js

---
+++ server/client/src/utils/toast.js
@@ -0,0 +1,25 @@
+import { toast } from 'react-toastify';
+
+export const showSuccess = (message) => {
+  toast.success(message, {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+export const showError = (message) => {
+  toast.error(message, {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
